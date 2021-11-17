39e8eda2b1ac5fe96aacc36b5ec67aaea6e2a419
krunam <knvises1@gmail.com>
2021-11-17 16:06:57 +0700

    edit

--- work_vue_2564/pages/index.vue
+++ work_vue_2564/pages/index.vue
@@ -23,18 +23,63 @@
             sm="6"
             md="4"
           >
-            <v-text-field
-              label="PLEATE ENTER YOUR PASSWORD"
-              placeholder="PASSWORD"
-              filled
-              rounded
-              dense
-            />
+            <v-menu transition="slide-y-reverse-transition">
+              <template #activator="{ on, attrs }">
+                <v-text-field
+                  label="PLEATE ENTER YOUR PASSWORD"
+                  placeholder="PASSWORD"
+                  filled
+                  rounded
+                  dense
+                  v-bind="attrs"
+                  v-on="on"
+                />
+              </template>
+              <div class="box">
+                <v-row
+                  v-for="n in 3"
+                  :key="n"
+                >
+                  <v-col
+                    v-for="k in 3"
+                    :key="k"
+                    md="3"
+                  >
+                    <v-btn
+                      class="mx-2"
+                      fab
+                      dark
+                      color="primary"
+                    >
+                      <p>1</p>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </div>
+              <!-- <v-list>
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-btn depressed>
+                    <v-list-item-title v-text="n" />
+                  </v-btn>
+                </v-list-item>
+              </v-list> -->
+            </v-menu>
             <v-btn class="btn1">
               <span class="txt1">LOG IN</span>
             </v-btn>
             <p>Forgot password?</p>
             <p>New to Bank Apps? Sign Up</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+            </v-menu>
           </v-col>
         </v-row>
       </v-container>
@@ -55,5 +100,12 @@ h3 {
 .btn1{
    background: linear-gradient(to bottom,#FFEB3B,#FFC107);
    margin-bottom: 60px;
+}
+.box{
+  background-color:white;
+  padding:5px;
+  border-radius: 25px;
+  text-align: center;
+  font-size: 18px;
 }
 </style>
